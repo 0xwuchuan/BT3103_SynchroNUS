7a6b2f92664e77c3c96b6b07bddc3a779b56a34f
Khoo Wu Chuan <wuchuank@gmail.com>
2022-03-12 18:10:46 +0800

    Login component & installed firebase

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="flex justify-center bg-blue-800 h-screen">
-    
+  <div class="flex justify-center bg-blue-800 items-center h-screen">
+    <Login />
   </div>
 </template>
 
 <script>
+import Login from './components/Login.vue'
 
 export default {
   name: 'App',
   components: {
+    Login,
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
+// import firebase and create firebase.js
 
 createApp(App).mount("#app");
