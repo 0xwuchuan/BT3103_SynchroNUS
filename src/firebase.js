d75215afe030caba3acf1e536c224ec8d12ec044
Xing Wei <xingwei.cheang@u.nus.edu>
2022-03-13 14:48:46 +0800

    Added Firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCCqJdEasU5i9peInaVSjguvk3cO4uZYD4",
+  authDomain: "synchronus-5906f.firebaseapp.com",
+  projectId: "synchronus-5906f",
+  storageBucket: "synchronus-5906f.appspot.com",
+  messagingSenderId: "626768028392",
+  appId: "1:626768028392:web:bf2e23b60d57f2fd7f5f5d",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
