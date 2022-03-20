b1afce4d86dad6591c26820db68df5c72ed0588a
Khoo Wu Chuan <wuchuank@gmail.com>
2022-03-20 18:16:30 +0800

    fix styling abit

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="sticky w-full top-0 p-5 flex justify-between">
+    <nav class="sticky w-full top-0 p-5 flex justify-between bg-gradient-to-r from-primary to-secondary">
         <router-link v-if="user" class="text-xl" to="/home">
             <img class="w-72" src="../assets/3.1.png" alt="Logo">
         </router-link>
