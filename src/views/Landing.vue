1e7142f21f362b3b3ddbd628aa08eda42ce8ad97
Khoo Wu Chuan <wuchuank@gmail.com>
2022-04-02 23:17:46 +0800

    Landing page

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,17 +1,23 @@
 <template>
-  <Nav />
-  <div class="flex flex-col justify-center items-center h-5/6">
-      <p class="text-white text-3xl">Welcome to SynchroNUS, <router-link class="text-primary" to='/login'>login here</router-link></p>
+  <div class="flex flex-col justify-center items-center h-full">
+      <img src="../assets/3.1.png" alt="Logo">
+      <p class="text-white text-xl font-bold">
+        SynchroNUS is a platform for NUS students to seek like-minded peers by publishing events on the platform 
+      </p>
+      <router-link class="text-primary" to='/login'>
+        <button class="rounded-lg bg-secondary px-5 py-2 font-bold text-black text-xl font-mont
+            hover:bg-opacity-80 transition duration-150 ease-linear">
+            Start now
+        </button>
+      </router-link>
   </div>
 </template>
 
 <script>
-import Nav from "../components/Nav.vue"
 
 export default {
     name: 'Landing',
     components: {
-        Nav
     }
 }
 </script>
