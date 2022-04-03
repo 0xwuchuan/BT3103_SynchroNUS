f60cd60ddc6a2da77177bda8e0032c9af18a27ae
Khoo Wu Chuan <wuchuank@gmail.com>
2022-04-03 18:04:28 +0800

    Dynamic display of images

--- src/components/SkeletonFilter.vue
+++ src/components/SkeletonFilter.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="!isLoading" class="flex flex-row items-center w-23/24 bg-white bg-opacity-30 rounded-lg m-4 
+    <div class="flex flex-row items-center w-23/24 bg-white bg-opacity-30 rounded-lg m-4 
         hover:bg-opacity-100 hover:shadow-2xl transition duration-200 ease-linear"> 
         <span class="h-16"></span>    
     </div>
