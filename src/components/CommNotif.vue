1c1baa16a8269523a3fe95c544006f13df4d90fe
nicolegohw <e0564290@u.nus.edu>
2022-04-03 18:23:47 +0800

    added links for notifications

--- src/components/CommNotif.vue
+++ src/components/CommNotif.vue
@@ -2,60 +2,21 @@
 
     <div
     class="p-2 rounded text-700 bg-50 border-900/10 w-100">
+    <a :href="link" class="text-black">
     
-    <b>{{ user }}</b> commented on your event: {{ text }}
+    <b>{{ user }}</b> commented on your event: {{ text }} 
+    </a>
     </div><hr>
-
-    <!---<div
-    class="p-4 border rounded text-700 bg-50 border-900/10 w-100 object-position:left"
-    role="alert"
-    >
-    <strong class="text-sm font-medium"> User commented on your event </strong>
-    </div>
-
-    <div
-    class="p-4 text-800 border rounded border-900/10 bg-50 w-100"
-    role="alert"
-    >
-    <strong class="text-sm font-medium"> Your request to join xx was accepted </strong>
-    </div>
-
-    <div
-    class="p-4 border rounded text-700 bg-50 border-900/10 w-100"
-    role="alert"
-    >
-    <strong class="text-sm font-medium"> User requested to join your event </strong>
-    </div>
-
-</div>
-    if commented
-    <div class="text">
-        <h5>User commented on your event</h5>
-    </div>
-
-    
-    <div class="text">
-        <h5>User requested to join your event</h5>
-    </div>
-
-   
-    <div class="text">
-        <h5>Your request to join Event was accepted</h5>
-    </div>-->
-  
 
 </template>
 
 <script>
-
-// import { getAuth } from "firebase/auth";
-
-
 export default {
-    name: 'singleNotif',
+    name: 'CommNotif',
     props: {
         user: {required: true, type: String},
-        text: {required: true, type: String}
+        text: {required: true, type: String},
+        link: {required: true}
     }
 
 }   
@@ -75,6 +36,13 @@ export default {
     background-color: #F2F2F2;
     border-radius: 30px;
     box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.2);
+}
+
+a {
+    text-decoration: none;
+}
+a:hover {
+    color: rgb(7 89 133);
 }
 
 </style>
