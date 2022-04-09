2c340f4f0bcfe6f5a1e1c8f48ac6bc807a48a1f2
Khoo Wu Chuan <wuchuank@gmail.com>
2022-04-09 14:54:20 +0800

    Styling changes for Edit Profile

--- src/views/EditProfile.vue
+++ src/views/EditProfile.vue
@@ -1,13 +1,13 @@
 <template>
     <Nav />   
-    <div class="flex justify-center flex-row h-5/6 ml-10 p-5">
-        <div class="flex justify-center flex-col bg-white opacity-90 rounded-lg filter drop-shadow-md h-108 w-10/12 md:w-104 p-5">
-            <div class="flex justify-end p-2">
+    <div class="flex justify-center items-center flex-row h-5/6 ml-10 p-5">
+        <div class="flex justify-center flex-col bg-white opacity-90 rounded-lg filter drop-shadow-md h-100 w-10/12 md:w-104 p-5">
+            <div class="flex justify-between p-2">
+            <h3 class="text-3xl text-center font-semibold pl-3 ml-8">Edit your Profile</h3>
             <button @click="backToProfile" type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white">
                 <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
             </button>
             </div>
-            <h3 class="text-3xl text-center font-semibold pl-3">Edit your Profile</h3>
             <form id="editForm" class="flex flex-col justify-center items-center w-full" @submit.prevent="editUser()">
                 <div class="flex flex-col items-left w-10/12 m-3">
                     <div class="flex flex-row items-center">
